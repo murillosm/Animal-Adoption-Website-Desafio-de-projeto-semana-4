7fa65a091a78d9f0cbd40c2b570a5125610ed556
Murillo  Sousa <murillosm@hotmail.com>
2024-05-31 00:24:00 -0300

    main screen concluido

---
+++ assets/js/header.js
@@ -0,0 +1,8 @@
+const head = document.head;
+const body = document.body;
+
+const style = '<link rel="stylesheet" type="text/css" href="assets/css/global/header.css" />';
+
+head.innerHTML += style;
+
+const header = document.createElement('header');
