9bde7692d3c268fc7b71ffc681307921aae91885
Murillo  Sousa <murillosm@hotmail.com>
2024-06-04 15:24:52 -0300

    escrita do readme

--- assets/js/header.js
+++
@@ -1,8 +0,0 @@
-const head = document.head;
-const body = document.body;
-
-const style = '<link rel="stylesheet" type="text/css" href="assets/css/global/header.css" />';
-
-head.innerHTML += style;
-
-const header = document.createElement('header');
--- assets/js/success_page.js
+++
@@ -1,4 +0,0 @@
-// Chama a função para inicializar o evento de clique
-
-
-addEventListener(initializeButtonClick());
